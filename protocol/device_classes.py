838aa4b40368a483423c9415a12d1164398a510b
dhilanpatel26 <dpatel93@jhu.edu>
2024-05-19 17:32:06 -0400

    Added DeviceList class

--- protocol/device_classes.py
+++ protocol/device_classes.py
@@ -11,9 +11,9 @@ class Device:
         """
         self.id = id  # unique device identifier, randomly generated
         self.leader = False  # initialized as follower
-        self.starter = False  # starter/reserve status
         self.received = None  # holds most recent message payload
         self.missed = 0  # number of missed check-ins, used by current leader
+        self.task = -1  # task identifier, -1 denotes reserve
 
     def get_id(self) -> int:
         """
@@ -27,12 +27,6 @@ class Device:
         """
         return self.leader
 
-    def get_starter(self):
-        """
-        :return: Device's starter/reserve status.
-        """
-        return self.starter
-
     def get_received(self) -> int:
         """
         :return: Device's most recently received message.
@@ -41,9 +35,15 @@ class Device:
 
     def get_missed(self) -> int:
         """
-        :return: Number of missed check-ins for this Device.
+        :return: number of missed check-ins for this Device.
         """
         return self.missed
+
+    def get_task(self) -> int:
+        """
+        :return: device's current task identifier
+        """
+        return self.task
 
     def make_leader(self):
         """
@@ -57,23 +57,147 @@ class Device:
         """
         self.leader = False
 
-    def make_starter(self):
-        """
-        Makes this Device a starter.
-        """
-        self.starter = True
-
-    def make_reserve(self):
-        """
-        Makes this Device a reserve.
-        :return:
-        """
-        self.starter = False
-
     def incr_missed(self) -> int:
         """
         Increments number of missed check-ins by 1.
-        :return: New number of missed check-ins.
+        :return: new number of missed check-ins.
         """
         self.missed += 1
         return self.missed
+
+    def set_task(self, task: int):
+        """
+        Sets the task for this Device.
+        :param task: task's int identifier
+        """
+        self.task = task
+
+
+class ThisDevice(Device):
+    pass
+
+
+class DeviceList:
+    """ Container for lightweight Device objects, held by ThisDevice. """
+
+    def __init__(self, num_tasks=8):
+        """
+        Non-default constructor for DeviceList object.
+        :param num_tasks: size of DeviceList, number of tasks.
+        """
+        self.devices = []
+        self.task_options = list(range(num_tasks))
+
+    def __str__(self):
+        """
+        String representation of Devices in DeviceList.
+        :return: concatenated string representation.
+        """
+
+        output = ["DeviceList:"]
+        for device in self.devices:
+            task = device.get_task() if device.get_task() != -1 else "Reserve"
+            output.append(f"Device ID: {hex(device.get_id())}, Task: {task}")
+        return "\n\t".join(output)
+
+    def __iter__(self):
+        """
+        Iterator for Devices in DeviceList.
+        :return: iterator object.
+        """
+        return iter(self.devices)
+
+    def __len__(self):
+        """
+        Length of Devices in DeviceList.
+        :return: number of Devices in DeviceList as an int.
+        """
+        return len(self.devices)
+
+    def update_num_tasks(self, num_tasks: int):
+        """
+        Resize DeviceList, used to upscale or downscale tasks.
+        :param num_tasks: number of tasks in new operation.
+        """
+        self.task_options = list(range(num_tasks))
+
+    def add_device(self, id: int, task: int):
+        """
+        Creates Device object with id and task, stores in DeviceList.
+        :param id: identifier for device, assigned to new Device object.
+        :param task: task for device, assigned to new Device object.
+        """
+        device = Device(id)
+        device.set_task(task)
+        self.devices.append(device)
+
+    def find_device(self, id: int) -> int or None:
+        """
+        Finds Device object with target id in DeviceList.
+        :param id: identifier for target device.
+        :return: Device object if found, None otherwise.
+        """
+        for device in self.devices:
+            if device.get_id() == id:
+                return device
+        return None
+
+    def remove_device(self, id: int) -> bool:
+        """
+        Removes Device object with target id in DeviceList.
+        :param id: identifier for target device
+        :return: True if found and removed, False otherwise.
+        """
+
+        device = self.find_device(id)
+        if device:
+            self.devices.remove(device)
+            return True
+        return False
+
+    def unused_tasks(self) -> [int]:
+        """
+        Gets list of tasks not currently assigned to a device.
+        :return: list of unused task indices.
+        """
+        unused_tasks = self.task_options.copy()
+        for d in self.devices:
+            if d.get_task() != -1 and d.get_task() in unused_tasks:
+                unused_tasks.remove(d.get_task())
+        return unused_tasks
+
+    def get_reserves(self):
+        """
+        Gets list of reserve devices (not currently assigned a task).
+        :return: list of reserve devices.
+        """
+        reserves = []
+        for d in self.devices:
+            if d.get_task() == -1:
+                reserves.append(d)
+        return reserves
+
+    def update_task(self, id: int, task: int):
+        """
+        Reassigns task to target device.
+        :param id: identifier for target device.
+        :param task: new task to be assigned to target.
+        """
+        for device in self.devices:
+            if device.get_id() == id:
+                device.set_task(task)  # pass-by-reference enhanced for allows this?
+        # exception
+
+    def get_highest_id(self) -> Device:
+        """
+        Gets Device with the largest id, used for leader takeover and tiebreaker.
+        :return: Device object with the largest id
+        """
+        max_device = None
+        max_id = 0
+        for device in self.devices:
+            if device.get_id() > max_id:
+                max_id = device.get_id()
+                max_device = device  # reference to max Device object
+        return max_device
+
