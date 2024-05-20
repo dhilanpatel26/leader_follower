28bf6028f46be456a09485a2b7dfe28f6f63f938
Kayleigh-Huk <kayleigh@hukdivito.org>
2024-05-20 13:23:46 -0400

    add testing TODOs for message class

--- tests/test_message_classes.py
+++ tests/test_message_classes.py
@@ -4,17 +4,27 @@ sys.path.append('../protocol')
 
 from message_classes import Message
 
-
 class TestMessageClass(unittest.TestCase):
 
     def setUp(self):
         pass
 
-    def test_constructor(self):
+    def test_basic_constructor(self):
+        # basic test
         basic_msg = Message(1, 0, 0, 0, 0)
         corr_msg = 1e26
         message = "Basic constructor message not equal to 1e26"
         self.assertEqual(basic_msg.msg, corr_msg, message)
+
+    # TODO: test invalid action code - not implemented in message yet
+    # useful to have to identify dropped packets
+    def test_invalid_action(self):
+        pass
+
+    # TODO: test negative options (task assignment = reserve) - not implemented in message yet
+    def test_negative_option(self):
+        pass
+
 
 if __name__ == '__main__':
     unittest.main()
