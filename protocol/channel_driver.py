00a9ccf6bb1eed54eefb559e07d3fda7f69e3bf4
dhilanpatel26 <dpatel93@jhu.edu>
2024-07-06 16:35:38 -0400

    Async websocket client integrated into SimulationNode (synchronous protocol still runs)

--- protocol/channel_driver.py
+++ protocol/channel_driver.py
@@ -1,9 +1,10 @@
 import time
 from simulation_network import SimulationNode, NetworkVisualizer, Network
 import itertools
+import asyncio
 
 
-def main():
+async def main():
     """
     Main driver for protocol simulation.
     :return:
@@ -12,9 +13,11 @@ def main():
     num_devices = 1
     network = Network()
     nodes = []
+    init_tasks = []
     for i in range(num_devices):
         new_node = SimulationNode(i+1)
         nodes.append(new_node)
+        init_tasks.append(new_node.async_init())
         network.add_node(new_node.node_id, new_node)
 
     for i in range(len(nodes)):
@@ -31,6 +34,8 @@ def main():
         node.start()
         time.sleep(5)
 
+    await asyncio.gather(*init_tasks)
+
 
 if __name__ == "__main__":
-    main()
+    asyncio.run(main())
