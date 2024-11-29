dfa2edd9e847920cea7fd45571857a5a44eb27ab
Kayleigh-Huk <kayleigh@hukdivito.org>
2024-11-29 13:02:23 -0500

    untested pub/sub methods + send/receive implemented

--- protocol/zigbee_network.py
+++ protocol/zigbee_network.py
@@ -3,14 +3,14 @@ import json
 from abstract_network import AbstractTransceiver, AbstractNode
 import multiprocessing
 import device_classes as dc
-
+from queue import Queue
+import time
 
 class ZigbeeNode(AbstractNode):
      
     def __init__(self, node_id, target_func = None, target_args = None, active: multiprocessing.Value = None):  # type: ignore
         self.transceiver = ZigbeeTransceiver(broker_address='localhost', broker_port=1883)  # address and port subject to change
         self.setup(node_id, target_func, target_args, active)
-
 
 class ZigbeeTransceiver(AbstractTransceiver):
     
@@ -19,13 +19,27 @@ class ZigbeeTransceiver(AbstractTransceiver):
         self.broker_port = broker_port
         self.username = username
         self.password = password
-        self.client = mqtt.Client()
-        
+        self.sub_client = mqtt.Client()
+        self.pub_client = mqtt.Client()
+        self.sub_topic = 'zigbee2mqtt/bridge/request/device/permit_join'
+        self.rcv_queue = Queue()
+
         if username and password:
-            self.client.username_pw_set(username, password)
+            self.sub_client.username_pw_set(username, password)
+            self.pub_client.username_pw_set(username, password)
         
-        self.client.connect(self.broker_address, self.broker_port)
-        self.client.loop_start()  # Start the loop in a separate thread
+        # sub loop callback funcs
+        self.sub_client.on_connect = self.on_sub_connect
+        self.sub_client.on_message = self.on_message
+        
+        # start sub loop
+        self.sub_client.user_data_set(self.rcv_queue)
+        self.sub_client.connect(self.broker_address, self.broker_port)
+        self.sub_client.loop_start()  # Start the loop in a separate thread
+
+        # start pub loop
+        self.pub_client.connect(self.broker_address, self.broker_port)
+        self.pub_client.loop_start()  # Start the loop in a separate thread
 
     def send(self, msg, topic='zigbee2mqtt/bridge/request/device/permit_join'):
         # Convert the message to JSON if it's a dictionary
@@ -33,8 +47,26 @@ class ZigbeeTransceiver(AbstractTransceiver):
             msg = json.dumps(msg)
         
         # Publish the message to the specified topic
-        self.client.publish(topic, msg)
+        # qos=1 waits for acknowledgement from broker  (we could do 0 which has no wait)
+        msg_info = self.client.publish(topic, msg, qos=1)
+        # rc is at index 0 of msg_info - will be 0 if success
+        # we could use this to decide whether we should resend or not?
+    
+    def receive(self, timeout):
+        # sub client is already subscribed, we just wait for queue to receive
+        end = time.time() + timeout
+        msg = self.rcv_queue.get(timeout=timeout)
+        return msg
 
     def __del__(self):
         self.client.loop_stop()  # Stop the loop when the object is deleted
         self.client.disconnect()
+
+    def on_message(self, client, userdata, message):
+        # we want to put any message received since protocol will
+        # handle whether it is valid or not
+        # we could change this setup to allow for checks here which may be faster
+        userdata.put(self, message.payload)
+
+    def on_sub_connect(self, client, userdata, flags, reason_code, properties):
+        client.subscribe(self.sub_topic)
